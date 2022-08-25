82cc95d13cbdb771ca0a094c1d5945dfb3502476
Jessie Baron <jessiebaron.political@gmail.com>
2022-08-25 19:27:51 -0400

    get the current user session

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -28,6 +28,18 @@ router.post(
       });
     }
   );
-
+  
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
 
   module.exports = router;
