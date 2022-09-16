8e63a64ccb43d59b674653b912a5b2081681cbad
Jessie Baron <jessiebaron.political@gmail.com>
2022-09-16 16:22:38 -0400

    setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf'
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
