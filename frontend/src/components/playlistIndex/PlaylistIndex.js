849c5ff81661cbd6807c034f2b240c1029bd3581
Jessie Baron <jessiebaron.political@gmail.com>
2022-09-20 20:25:50 -0400

    ?

--- frontend/src/components/playlistIndex/PlaylistIndex.js
+++ frontend/src/components/playlistIndex/PlaylistIndex.js
@@ -1,34 +1,32 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { getPlaylistByUser } from '../../store/playlist';
 import './PlaylistIndex.css'
 
 const PlaylistIndex = () => {
     const dispatch = useDispatch()
-    const currentUser = useSelector(state => state.session.user)
-    console.log("this is the users name", currentUser)
 
-    useEffect(() => {
-      dispatch(getPlaylistByUser(currentUser))
-    }, [])
+    const currentUser = useSelector(state => state.session.user)
+    const playlist = dispatch(getPlaylistByUser(currentUser))
+    console.log("this is the playlist", playlist)
 
     const playlistObject = useSelector(state => state.playlist)
     console.log("this is the playlistObj", playlistObject)
-    const playlists = Object.values(playlistObject)
-    console.log("this is the playlists array", playlists)
+    console.log("this is the currentUser Object", currentUser)
 
     return (
         <div className='playlistSection'>
+          {/* <ul>
             {
-              playlists.map(playlist => (
-                <div>
-                <li>{playlist.imageUrl}</li>
-                <h4>{playlist.name}</h4>
-                </div>
+              playlist.map(playlist => (
+                <playlistIndexItem
+                  playlist={playlist}
+                  key={playlist.id}
+                />
               ))
             }
+          </ul> */}
           <Link to="/playlist/new">Add New Playlist</Link>
         </div>
       );
