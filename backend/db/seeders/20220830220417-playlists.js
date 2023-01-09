9f6c7e166475735669459b3f15859548aa745bc3
Jessie Baron <jessiebaron.political@gmail.com>
2023-01-09 17:51:49 -0500

    updating playlists

--- backend/db/seeders/20220830220417-playlists.js
+++ backend/db/seeders/20220830220417-playlists.js
@@ -4,7 +4,6 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -72,17 +71,17 @@ module.exports = {
         imageUrl: "image.2"
       },
       {
-        userId: 2,
+        userId: 1,
         name: "Jazz Playlist",
         imageUrl: "image.3"
       },
       {
-        userId: 3,
+        userId: 2,
         name: "Country Playlist",
         imageUrl: "image.4"
       },
       {
-        userId: 3,
+        userId: 2,
         name: "Rap Playlist",
         imageUrl: "image.5"
       },
